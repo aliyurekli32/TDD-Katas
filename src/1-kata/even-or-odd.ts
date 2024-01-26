40c0ff92dc9a74dc7d917b16a2c10ad1973d29ea
aliyurekli <aliyurekli@dekatechs.com>
2024-01-27 01:38:53 +0300

    working

--- src/1-kata/even-or-odd.ts
+++ src/1-kata/even-or-odd.ts
@@ -1,15 +1,13 @@
-export const even_or_odd = (numbers: number[]): string => {
-  const sum = (numbers: number[]): number => {
-    if (numbers.length === 0) {
-      return 0;
-    } else if (numbers.length === 1) {
-      return numbers[0];
-    }
-    return sum(numbers.slice(1));
-  };
-  if (sum(numbers) % 2 === 0) {
-    return "even";
-  } else {
-    return "odd";
-  }
-};
+const pipe =
+  (...fns: Function[]) =>
+  (x: any) =>
+    fns.reduce((acc, fn) => fn(acc), x);
+
+const sum = (numbers: number[]): number =>
+  numbers.reduce((acc: number, num: number) => acc + num, 0);
+
+const isEven = (n: number): boolean => n % 2 === 0;
+
+const toEvenOrOddString = (isEven: boolean): string =>
+  isEven ? "even" : "odd";
+export const evenOrOdd = pipe(sum, isEven, toEvenOrOddString);
