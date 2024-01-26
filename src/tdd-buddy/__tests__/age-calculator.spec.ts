1ed1cc24f6d3ea25cdfc3e376c1eb957f98445ff
aliyurekli <aliyurekli@dekatechs.com>
2024-01-27 00:36:25 +0300

    working

--- src/tdd-buddy/__tests__/age-calculator.spec.ts
+++ src/tdd-buddy/__tests__/age-calculator.spec.ts
@@ -1,7 +1,7 @@
 import { AgeCalculator } from "../age-calculator";
 
 describe("AgeCalculator", () => {
-  test("should calculate age correctly", () => {
+  test.skip("should calculate age correctly", () => {
     const ageCalculator = new AgeCalculator();
     const age1 = ageCalculator.calculateAge(
       "28 October 2016",
@@ -21,7 +21,7 @@ describe("AgeCalculator", () => {
     expect(age3).toBe(6);
   });
 
-  test("should throw an error if birth date is after current date", () => {
+  test.skip("should throw an error if birth date is after current date", () => {
     const ageCalculator = new AgeCalculator();
     const age = ageCalculator.calculateAge(
       "28 October 2022",
@@ -30,14 +30,14 @@ describe("AgeCalculator", () => {
     expect(age).toBe("Invalid date format");
   });
 
-  test("should return September 3, 2017; If I was born on September 9, 2017", () => {
+  test.skip("should return September 3, 2017; If I was born on September 9, 2017", () => {
     const ageCalculator = new AgeCalculator();
     const startDateOfBirthdayWeek =
       ageCalculator.calculateStartDateOfBirthdayWeek("September 9, 2017");
 
     expect(startDateOfBirthdayWeek).toBe("September 3, 2017");
   });
-  test("should return August 28, 2017; If I was born on September 3, 2017", () => {
+  test.skip("should return August 28, 2017; If I was born on September 3, 2017", () => {
     const ageCalculator = new AgeCalculator();
     const startDateOfBirthdayWeek =
       ageCalculator.calculateStartDateOfBirthdayWeek("September 3, 2017");
