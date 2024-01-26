e112e78cfea061286b7e005813faff865ea0da1c
aliyurekli <aliyurekli@dekatechs.com>
2024-01-27 01:04:29 +0300

    update: remove spec files

--- src/1-kata/even-or-odd.spec.ts
+++
@@ -1,7 +0,0 @@
-import { even_or_odd } from "./even-or-odd";
-
-describe("Even or Odd", () => {
-  it("should return 'even' if the number is even", () => {
-    expect(even_or_odd(2)).toBe("even");
-  });
-});
