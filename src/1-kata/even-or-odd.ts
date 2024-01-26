ba2c68a22ad5d210c6ec66417ba2196c34b49b99
aliyurekli <aliyurekli@dekatechs.com>
2024-01-27 01:15:13 +0300

    working

--- src/1-kata/even-or-odd.ts
+++ src/1-kata/even-or-odd.ts
@@ -1,6 +1,15 @@
-export const even_or_odd = (number: number): string => {
-  if (number % 2 === 0) {
+export const even_or_odd = (numbers: number[]): string => {
+  const sum = (numbers: number[]): number => {
+    if (numbers.length === 0) {
+      return 0;
+    } else if (numbers.length === 1) {
+      return numbers[0];
+    }
+    return sum(numbers.slice(1));
+  };
+  if (sum(numbers) % 2 === 0) {
     return "even";
+  } else {
+    return "odd";
   }
-  return "odd";
 };
