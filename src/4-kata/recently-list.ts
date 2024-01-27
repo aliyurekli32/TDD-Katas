0ec0c59ed4d213da82d085d2751960f2da9bcb6d
aliyurekli <aliyurekli@dekatechs.com>
2024-01-27 20:41:07 +0300

    working

--- src/4-kata/recently-list.ts
+++ src/4-kata/recently-list.ts
@@ -1,6 +1,9 @@
 export class RecentlyList {
   private list: number[] = [];
 
+  addItemToList(number: number) {
+    this.list = [number, ...this.list];
+  }
   getRecentlyList() {
     return this.list;
   }
