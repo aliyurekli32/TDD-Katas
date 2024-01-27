7929a2c719df9cfefd1d6472022206ded526cbe7
aliyurekli <aliyurekli@dekatechs.com>
2024-01-28 00:19:45 +0300

    working

--- src/5-kata/bowling-game.ts
+++ src/5-kata/bowling-game.ts
@@ -4,6 +4,20 @@ export class BowlingGame {
     this.list.push(number);
   }
   score() {
-    return this.list.reduce((a, b) => a + b, 0);
+    let takeIndex: boolean = true;
+    return this.list.reduce((sum: number, num: number, index: number) => {
+      if (!takeIndex) {
+        takeIndex = true;
+        return sum;
+      }
+      if (this.list[index] + this.list[index + 1] === 10) {
+        takeIndex = false;
+        return sum + 10 + this.list[index + 2];
+      } else if (this.list[index] + this.list[index + 1] < 10) {
+        takeIndex = false;
+        return sum + num + this.list[index + 1];
+      }
+      return sum;
+    }, 0);
   }
 }
