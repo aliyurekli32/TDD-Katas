144ab0ecf448c545ab518cd3fc970e71f20affee
aliyurekli <aliyurekli@dekatechs.com>
2024-01-27 12:29:26 +0300

    working

--- src/3-kata/calculate-stats.ts
+++ src/3-kata/calculate-stats.ts
@@ -1,5 +1,8 @@
-export const calculateStats = (numbers: number[]): { min: number } => {
+export const calculateStats = (
+  numbers: number[]
+): { min: number; max: number } => {
   return {
     min: Math.min(...numbers),
+    max: Math.max(...numbers),
   };
 };
