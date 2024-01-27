ff8a1aa59a5f039a97cac65ddfa412f44b122c95
aliyurekli <aliyurekli@dekatechs.com>
2024-01-27 21:53:35 +0300

    working

--- src/4-kata/recently-list.ts
+++ src/4-kata/recently-list.ts
@@ -1,28 +1,32 @@
+type NumberOrString = number | string;
+
 export class RecentlyList {
-  private list: (number | string)[] = [];
+  private list: NumberOrString[] = [];
   listSize: number;
+
   constructor(listSize: number = 5) {
-    this.listSize = listSize < 0 ? 5 : listSize;
+    this.listSize = Math.max(listSize, 5);
   }
 
-  addItemToList(number: number | string) {
-    if (
-      !this.list.includes(number) &&
-      number !== null &&
-      number !== undefined &&
-      number !== "" &&
-      this.list.length < this.listSize
-    ) {
+  addItemToList = (number: NumberOrString): void => {
+    if (this.isEligibleToAdd(number)) {
       this.list = [number, ...this.list];
     }
-  }
-  getRecentlyList() {
-    return this.list;
-  }
-  gerElementWithIndex(index: number) {
+  };
+
+  getRecentlyList = (): NumberOrString[] => this.list;
+
+  getElementWithIndex = (index: number): NumberOrString => {
     if (index < this.list.length && index >= 0) {
       return this.list[index];
     }
     throw new Error("Invalid index");
-  }
+  };
+
+  private isEligibleToAdd = (number: NumberOrString): boolean =>
+    !this.list.includes(number) &&
+    number !== null &&
+    number !== undefined &&
+    number !== "" &&
+    this.list.length < this.listSize;
 }
