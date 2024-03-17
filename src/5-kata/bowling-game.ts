f3b9bd53e2c3abd8337808fec604f837f8b1ddc8
aliyurekli <aliyurekli@dekatechs.com>
2024-03-17 22:39:50 +0300

    working

--- src/5-kata/bowling-game.ts
+++ src/5-kata/bowling-game.ts
@@ -5,21 +5,19 @@ export class BowlingGame {
   }
   score() {
     let takeIndex: boolean = true;
+    const isStrike = (num: number, index: number, length: number) =>
+      num === 10 && index + 1 < length && index + 2 < length;
+    const isSpare = (num: number, index: number, length: number) =>
+      num + this.list[index + 1] === 10 && index + 2 < length;
+
     return this.list.reduce((sum: number, num: number, index: number) => {
       if (!takeIndex) {
         takeIndex = true;
         return sum;
       }
-      if (
-        num === 10 &&
-        index + 1 < this.list.length &&
-        index + 2 < this.list.length
-      )
+      if (isStrike(num, index, this.list.length))
         return sum + 10 + this.list[index + 1] + this.list[index + 2];
-      if (
-        this.list[index] + this.list[index + 1] === 10 &&
-        index + 2 < this.list.length
-      ) {
+      if (isSpare(num, index, this.list.length)) {
         takeIndex = false;
         return sum + 10 + this.list[index + 2];
       } else if (
